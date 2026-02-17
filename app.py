d947d1be5040dfccc22bb8fb606df4294611a6d3
deepasreevarmap <knowledgeofficer.5@ictkerala.org>
2026-02-17 20:50:37 +0530

    Version1 with dataset

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import joblib
+
+app = Flask(__name__)
+model = joblib.load("model.pkl")
+
+species_map = {0:"Setosa",1:"Versicolor",2:"Virginica"}
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    features = [
+        float(request.form["sepal_length"]),
+        float(request.form["sepal_width"]),
+        float(request.form["petal_length"]),
+        float(request.form["petal_width"])
+    ]
+
+    prediction = model.predict([features])[0]
+    result = species_map[prediction]
+
+    return render_template("index.html", prediction_text=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ train.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import joblib
+import mlflow
+
+# 1. Load Data
+iris = load_iris()
+data = pd.DataFrame(iris.data, columns=iris.feature_names)
+data["species"] = iris.target
+data.to_csv("iris.csv", index=False)
+
+X = data.drop("species", axis=1)
+y = data["species"]
+
+# 2. Split Data
+test_size = 0.2
+random_state = 42
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=test_size, random_state=random_state
+)
+
+# 3. MLflow Tracking Block
+with mlflow.start_run():
+    # --- LOG PARAMETERS (Settings you chose) ---
+    mlflow.log_param("test_size", test_size)
+    mlflow.log_param("random_state", random_state)
+    mlflow.log_param("max_iter", 300)
+
+    # --- TRAIN MODEL ---
+    model = LogisticRegression(max_iter=3)
+    model.fit(X_train, y_train)
+
+    # --- LOG METRICS (The results) ---
+    accuracy = model.score(X_test, y_test)
+    mlflow.log_metric("accuracy", accuracy)
+
+    # --- LOG ARTIFACTS (The actual files) ---
+    joblib.dump(model, "model.pkl")
+    mlflow.log_artifact("model.pkl")
+    mlflow.log_artifact("iris.csv")
+
+print(f"Model trained with accuracy: {accuracy}")
